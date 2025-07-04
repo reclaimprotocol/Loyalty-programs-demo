a2caf31fe06ec9ab25b7ea60985677a2ba58c607
koushith <koushith97@gmail.com>
2025-07-05 04:06:43 +0530

    fix deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import { Desktop } from './screens/Desktop/Desktop';
 import { ProgramList } from './screens/Programs/ProgramList';
 
 const App = () => {
   return (
-    <Router basename="/">
-      <Routes>
-        <Route path="/" element={<Desktop />}>
-          <Route path="/programs/:category" element={<ProgramList />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Desktop />}>
+        <Route path="/programs/:category" element={<ProgramList />} />
+      </Route>
+    </Routes>
   );
 };
 
