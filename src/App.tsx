74fa1d926253c188e0b3f9ebde1e9729be7d98b7
koushith <koushith97@gmail.com>
2025-07-05 06:28:49 +0530

    adds vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Desktop } from './screens/Desktop/Desktop';
 import { ProgramList } from './screens/Programs/ProgramList';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Desktop />}>
-        {/* Redirect root to airlines */}
-        <Route index element={<Navigate to="/programs/airlines" replace />} />
-        {/* Program routes */}
-        <Route path="/programs/:category" element={<ProgramList />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Desktop />}>
+          {/* Redirect root to airlines */}
+          <Route index element={<Navigate to="/programs/airlines" replace />} />
+          {/* Program routes */}
+          <Route path="/programs/:category" element={<ProgramList />} />
+        </Route>
+      </Routes>
+      <Analytics />
+    </>
   );
 };
 
